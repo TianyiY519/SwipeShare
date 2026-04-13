d9bb40a8cf4bfd2e0304cbb30d61c7b1d7a7e73a
TianyiY519 <tyang21@stevens.edu>
2026-04-13 00:58:58 -0400

    remove drf_yasg

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,32 +5,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-# Swagger/ReDoc API Documentation
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Fordham SwipeShare API",
-        default_version='v1',
-        description="API documentation for Fordham SwipeShare - Meal Swipe Exchange Platform",
-        terms_of_service="https://www.fordham.edu/",
-        contact=openapi.Contact(email="admin@fordham.edu"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 urlpatterns = [
     # Admin
     path('admin/', admin.site.urls),
-
-    # API Documentation
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/schema/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 
     # API Endpoints
     path('api/auth/', include('apps.users.urls')),

3df1b8b430378a8e4942d797ec8d4af2cf242317
TianyiY519 <tyang21@stevens.edu>
2026-04-13 01:40:09 -0400

    fix ALLOWED_HOSTS for Railway

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-sq5b(h)-730qz!mcdix$a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS += ['swipeshare-production.up.railway.app', '.railway.app']
 
 # Application definition
 INSTALLED_APPS = [
